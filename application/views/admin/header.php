31c2dd5d9e8cd716f78a7bd6b997e80ec1d5d01c
Prachisharma12345 <er.prachisharma10@gmail.com>
2018-06-18 16:56:33 +0530

    Add files via upload

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -11,4 +11,10 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" style="">
     <span class="navbar-toggler-icon"></span>
   </button>
+
+  <?php if($this->session->userdata('id')) 
+  		{
+  	?>
+  		<li><a href="<?= base_url('admin/logout'); ?>" class="btn btn-danger" style="">Logout</a></li>
+  	<?php	} ?>
   </nav>
