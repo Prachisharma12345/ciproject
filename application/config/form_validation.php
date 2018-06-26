4ecdcb5d3b1735d7525aebeb7b35b8eaab9a5545
Prachisharma12345 <er.prachisharma10@gmail.com>
2018-06-26 04:29:38 -0700

    Add files via upload

---
+++ application/config/form_validation.php
@@ -0,0 +1,28 @@
+<?php
+$config=[
+	'add_article_rules'=>[
+					[
+
+					'feild' => 'title',
+					'label'=>'Article Title',
+					'rules'=>'required|alpha'
+
+					],
+					[
+
+					'feild'=>'body',
+					'label'=>'Article Body',
+					'rules'=>'required|alpha'
+					]
+
+					],
+
+
+
+						
+
+
+
+
+];
+?>
